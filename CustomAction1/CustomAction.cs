f96d47b819c2cca216357d16d7aedcada2520d6c
Stein-Inge Åsmul <glytzhkof@gmail.com>
2025-09-28 01:54:12 +0200

    Working to upgrade solution to WiX 6.

--- CustomAction1/CustomAction.cs
+++ CustomAction1/CustomAction.cs
@@ -1,24 +1,27 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using WixToolset.Dtf.WindowsInstaller;
 using System.Windows.Forms;
 
-namespace CustomAction2
+namespace CustomAction1
 {
     public class CustomActions
     {
         [CustomAction]
-        public static ActionResult TestCustomAction(Session session)
+        public static ActionResult MyCustomAction(Session session)
         {
             // System.Diagnostics.Debugger.Launch();
-            session.Log("Begin TestCustomAction");
+            session.Log("Begin MyCustomAction");
 
             string data = session["CustomActionData"];
 
             MessageBox.Show("We are in deferred mode custom action. We received this incoming text:\n\n" + data);
 
             return ActionResult.Success;
+
+            //session.Log("Begin CustomAction1");
+            //return ActionResult.Success;
         }
     }
 }
